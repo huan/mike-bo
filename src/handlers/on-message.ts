8d520c45950ed72c5bf5556f5a239996fb1d5489
Huan LI <zixia@zixia.net>
2019-12-20 16:16:26 +0800

    reply dong when receive ding

--- src/handlers/on-message.ts
+++ src/handlers/on-message.ts
@@ -9,4 +9,30 @@ export default async function onMessage (
   message : Message,
 ): Promise<void> {
   log.info('on-message', 'onMessage(%s)', message)
+
+  await dingDong.call(this, message)
+}
+
+async function dingDong (
+  this: Wechaty,
+  message: Message,
+) {
+  log.info('on-message', 'dingDong()')
+
+  const text = message.text()
+  const type = message.type()
+  const room = message.room()
+  // const from = message.from()
+  // const mentionLisrt = await message.mention()
+
+  if (room) {
+    return
+  }
+
+  if (type === Message.Type.Text) {
+    if (text.toLowerCase() === 'ding') {
+      await message.say('dong')
+    }
+  }
+
 }
