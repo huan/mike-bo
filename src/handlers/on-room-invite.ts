cb40c49f279ab3e00b7e9a6e3029851971bd482b
Huan LI <zixia@zixia.net>
2019-12-20 16:16:13 +0800

    automatic accept room invitations

--- src/handlers/on-room-invite.ts
+++ src/handlers/on-room-invite.ts
@@ -4,9 +4,20 @@ import {
   Wechaty,
 }                   from 'wechaty'
 
+import {
+  chatops,
+}                 from '../chatops'
+
 export default async function onRoomInvite (
   this           : Wechaty,
   roomInvitation : RoomInvitation
 ): Promise<void> {
   log.info('on-room-invite', 'onRoomInvite(%s)', roomInvitation)
+
+  const topic = await roomInvitation.topic()
+  const inviter = await roomInvitation.inviter()
+
+  await chatops(this, `recreived room invitation from ${inviter} to ${topic}`)
+  await roomInvitation.accept()
+  await chatops(this, 'accepted.')
 }
