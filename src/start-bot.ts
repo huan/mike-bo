a3f5f0227ad1251d524c20e608b7bc91900b7b4a
Huan LI <zixia@zixia.net>
2019-12-30 13:15:57 +0800

    change emoji

--- src/start-bot.ts
+++ src/start-bot.ts
@@ -29,7 +29,7 @@ export async function startBot (wechaty: Wechaty): Promise<void> {
   }
   const ONE_HOUR = 60 * 60 * 1000
   setInterval(heartbeat('💖'), ONE_HOUR)
-  wechaty.on('ready', heartbeat('🙋‍♂️'))
+  wechaty.on('ready', heartbeat('🙋'))
   wechaty.on('logout', heartbeat('😪'))
 
   const wtmp = Wtmp.instance()
