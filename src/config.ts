d0d01a64d6246bcbd5f5f9918cf29263a0f20091
Huan LI <zixia@zixia.net>
2020-01-01 02:25:35 +0800

    add Microsoft Ignite audiences

--- src/config.ts
+++ src/config.ts
@@ -31,6 +31,10 @@ export const WECHATY_DEVELOPERS_HOME_ROOM_ID_LIST = [
   '4335801863@chatroom',    // Wechaty Developers' Room 3
   '22396239792@chatroom',   // Wechaty Developers' Room 4
   '19112581505@chatroom',   // Wechaty Developers' Room 5
+  /* ******************** */
+  '17559195108@chatroom',   // Wechaty Ignite 1
+  '18171595067@chatroom',   // Wechaty Ignite 2
+  '19487389780@chatroom',   // Wechaty Ignite 3
 ]
 
 /**
