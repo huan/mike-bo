56560056c832ac00d3c90efc6183f7a7539df6ff
Huan LI (李卓桓) <zixia@zixia.net>
2020-05-03 16:34:42 +0800

    use plugin

--- src/start-bot.ts
+++ src/start-bot.ts
@@ -4,9 +4,13 @@ import {
 }                   from 'wechaty'
 
 import {
+  DingDong,
+  Heartbeat,
+}                   from 'wechaty-plugin-contrib'
+
+import {
   log,
 }                  from './config'
-import { Chatops }  from './chatops'
 import { Wtmp }     from './wtmp'
 
 export async function startBot (wechaty: Wechaty): Promise<void> {
@@ -24,14 +28,30 @@ export async function startBot (wechaty: Wechaty): Promise<void> {
     .on('room-join',    './handlers/on-room-join')
     .on('room-leave',   './handlers/on-room-leave')
 
-  const heartbeat = (emoji: string) => {
-    return () => Chatops.instance().heartbeat(emoji)
-  }
-  const ONE_HOUR = 60 * 60 * 1000
-  setInterval(heartbeat('[爱心]'), ONE_HOUR)
-  wechaty.on('login', heartbeat(`[太阳] ${wechaty.name()}`))
-  wechaty.on('ready', heartbeat(`[拳头] ${wechaty.name()}`))
-  wechaty.on('logout', heartbeat(`[月亮] ${wechaty.name()}`))
+  wechaty.use(
+    DingDong({
+      at   : true,
+      room : false,
+    }),
+    Heartbeat({
+      emoji: {
+        heartbeat : '[爱心]',
+        login     : '[太阳]',
+        logout    : '[月亮]',
+        ready     : '[拳头]',
+      },
+      intervalSeconds: 60 * 60, // 1 hour
+    }),
+  )
+
+  // const heartbeat = (emoji: string) => {
+  //   return () => Chatops.instance().heartbeat(emoji)
+  // }
+  // const ONE_HOUR = 60 * 60 * 1000
+  // setInterval(heartbeat('[爱心]'), ONE_HOUR)
+  // wechaty.on('login', heartbeat(`[太阳] ${wechaty.name()}`))
+  // wechaty.on('ready', heartbeat(`[拳头] ${wechaty.name()}`))
+  // wechaty.on('logout', heartbeat(`[月亮] ${wechaty.name()}`))
 
   const wtmp = Wtmp.instance()
   const loginWtmp = (user: Contact) => wtmp.login(user.name())
