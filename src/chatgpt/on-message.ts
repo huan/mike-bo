7e16d14ad2338550cbf4e5cbfa64609d85e24613
Huan (李卓桓) <zixia@zixia.net>
2022-12-07 07:47:34 +0800

    freemium 1 for credits

--- src/chatgpt/on-message.ts
+++ src/chatgpt/on-message.ts
@@ -3,11 +3,10 @@ import { ChatGPTAPIBuilder } from './chatgpt-api-builder.js'
 
 const chatGptApi = await ChatGPTAPIBuilder()
 
-const MAX_FREEMIUM_NUM = 3
+const DEFAULT_CREDIT = 1
 const MAX_PREMIUM_NUM_NOTICE = '发个红包热闹一下吧！'
 
-const users = {} as Record<string, number>
-const sponsors = {} as Record<string, boolean>
+const credits = {} as Record<string, number>
 
 export async function onMessage (this: Wechaty, message: Message): Promise<void> {
   const room = message.room()
@@ -16,12 +15,15 @@ export async function onMessage (this: Wechaty, message: Message): Promise<void>
   // const topic = await room.topic()
   // if (!/ChatGPT/.test(topic)) return
 
+  const talker = message.talker()
+  const credit = credits[talker.id] || 0
+
   /**
    * Check red packet
    */
-  const talker = message.talker()
   if (message.type() === this.Message.Type.RedEnvelope) {
-    sponsors[talker.id] = true
+    credits[talker.id] = (credits[talker.id] || 0) + 1
+    return
   }
 
   if (!(await message.mentionSelf())) return
@@ -29,15 +31,11 @@ export async function onMessage (this: Wechaty, message: Message): Promise<void>
   /**
    * Check freemium
    */
-  const counter = users[talker.id] || 0
-  users[talker.id] = counter + 1
-
-  if (counter >= MAX_FREEMIUM_NUM) {
-    if (!sponsors[talker.id]) {
-      await room.say(MAX_PREMIUM_NUM_NOTICE, talker)
-      return
-    }
+  if (credit <= 0 - DEFAULT_CREDIT) {
+    await room.say(MAX_PREMIUM_NUM_NOTICE, talker)
+    return
   }
+  credits[talker.id] = credit - 1
 
   /**
    * Only response in a room with "ChatGPT" in the topic and mentioned self
